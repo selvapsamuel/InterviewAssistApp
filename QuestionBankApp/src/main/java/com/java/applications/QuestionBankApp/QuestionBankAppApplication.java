556a13791d569c4f37837a4108eb7ad947553bc6
Selvakumar Samuel Dhanraj <selvakumar7784@gmail.com>
2019-04-24 17:13:35 +0530

    addiing initial project skeleton

---
+++ QuestionBankApp/src/main/java/com/java/applications/QuestionBankApp/QuestionBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.applications.QuestionBankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBankAppApplication.class, args);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # InterviewAssistApp
-InterviewAssistApp
+InterviewAssistApp is an application that aids the interviewer with question to ask the candidate for given topics.
+The app manages its own Question Bank . Included is a bulk question loader as well
+
